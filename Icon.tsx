11b3d2a88ad111fa7b938e90f61f89880e064677
Visnu <109976023+visnudeva@users.noreply.github.com>
2025-07-21 15:53:22 +0400

    Add files via upload

---
+++ Icon.tsx
@@ -0,0 +1,36 @@
+
+import React from 'react';
+
+interface IconProps {
+  name: 'play' | 'pause' | 'search' | 'volume-high' | 'volume-low' | 'volume-off' | 'radio-tower';
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const ICONS: Record<IconProps['name'], React.ReactNode> = {
+  play: <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />,
+  pause: <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />,
+  search: <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />,
+  'volume-high': <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />,
+  'volume-low': <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 8.25c0-1.423-.828-2.667-2.012-3.244M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />,
+  'volume-off': <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 9.75L19.5 12m0 0l2.25 2.25M19.5 12l2.25-2.25M19.5 12l-2.25 2.25m-10.5-3l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />,
+  'radio-tower': <path strokeLinecap="round" strokeLinejoin="round" d="M9 9.563C9 9.254 9.254 9 9.563 9h4.874c.309 0 .563.254.563.563v4.874c0 .309-.254.563-.563.563H9.563C9.254 15 9 14.746 9 14.437V9.564zm-2.25-5.313a.75.75 0 00-1.06 1.06L9.44 9h5.12l3.75-3.75a.75.75 0 00-1.06-1.06L12 7.94 9.81 5.75l-3.06-3.06zM12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25z" />
+};
+
+const Icon: React.FC<IconProps> = ({ name, className = 'w-6 h-6', style }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+      style={style}
+    >
+      {ICONS[name]}
+    </svg>
+  );
+};
+
+export default Icon;
